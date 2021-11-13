366f7e5c6b491b61c443aff93a028b24f0eda6b1
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-12 18:42:28 -0600

    updated test folder

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,27 +1,48 @@
-const Letter = require("../lib/Letter");
+const Employee = require("../lib/Employee");
 
-describe("Letter class", () => {
-  it("Characters that aren't digits or letters are instantly visible", () => {
-    expect(new Letter("?").visible).toBe(true);
-  });
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof(e)).toBe("object");
+});
 
-  it("toString returns _ for letters", () => {
-    expect(new Letter("a").toString()).toBe("_");
-  });
+test("Can set name via constructor arguments", () => {
+  const name = "Alice";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
 
-  describe("guess", () => {
-    it("Correct guess returns true", () => {
-      expect(new Letter("j").guess("j")).toBe(true);
-    });
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe(testValue);
+});
 
-    it("Incorrect guess returns false", () => {
-      expect(new Letter("j").guess("l")).toBe(false);
-    });
-  });
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
 
-  describe("getSolution", () => {
-    it("returns character", () => {
-      expect(new Letter("l").getSolution()).toBe("l");
-    });
-  });
+test("Can get name via getName()", () => {
+  const testValue = "Alice";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
+
+test("Can get id via getId()", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.getId()).toBe(testValue);
+});
+
+test("Can get email via getEmail()", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
+test("getRole() should return \"Employee\"", () => {
+  const testValue = "Employee";
+  const e = new Employee("Alice", 1, "test@test.com");
+  expect(e.getRole()).toBe(testValue);
 });
--- tests/Engeneer.test.js
+++ tests/Engeneer.test.js
@@ -1,39 +1,19 @@
-const Word = require("../lib/Word");
-const Letter = require("../lib/Letter");
+const Engineer = require("../lib/Engineer");
 
-describe("Word class", () => {
-  it("Creates an array of Letter objects", () => {
-    const word = new Word("hi");
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.github).toBe(testValue);
+});
 
-    expect(word.letters).toEqual(
-      expect.arrayContaining([expect.objectContaining({ char: "h" })])
-    );
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
 
-    expect(word.letters[0]).toBeInstanceOf(Letter);
-  });
-
-  describe("guessLetter", () => {
-    it("Correct guess returns true", () => {
-      expect(new Word("fish").guessLetter("i")).toBe(true);
-    });
-
-    it("Incorrect guess returns false", () => {
-      expect(new Word("fish").guessLetter("o")).toBe(false);
-    });
-  });
-
-  describe("guessedCorrectly ", () => {
-    it("returns true if all letters are correct", () => {
-      const word = new Word("hi");
-      word.guessLetter("h");
-      word.guessLetter("i");
-      expect(word.guessedCorrectly()).toBe(true);
-    });
-    it("returns false if at least one letter is incorrect", () => {
-      const word = new Word("hi");
-      word.guessLetter("h");
-      word.guessLetter("a");
-      expect(word.guessedCorrectly()).toBe(false);
-    });
-  });
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(e.getGithub()).toBe(testValue);
 });
